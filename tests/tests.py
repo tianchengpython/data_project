5c36d0ec8d0056e1767b846f8efe52c8d69785d8
tiancheng <tian41848@gmail.com>
2024-03-21 17:26:22 +0800

    1.9v

--- tests/tests.py
+++ tests/tests.py
@@ -1,40 +1,68 @@
 
-from transformers import BertTokenizer, BertForNextSentencePrediction
-import torch
+# from transformers import BertTokenizer, BertForNextSentencePrediction
+# import torch
 
+# if __name__ == '__main__':
+
+
+    # # 加载BERT模型和分词器
+    # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    # model = BertForNextSentencePrediction.from_pretrained('bert-base-uncased')
+    #
+    # # 定义关键词
+    # keyword = "apple"
+    #
+    # # 示例文本
+    # text = "I like bananas. Apples are delicious fruits. Orange is another fruit."
+    #
+    # # 分词并编码
+    # inputs = tokenizer(keyword, text, return_tensors='pt', padding=True, truncation=True)
+    #
+    # # 进行文本相似度预测
+    # outputs = model(**inputs)
+    #
+    # # 获取相似度分数
+    # score = torch.softmax(outputs.logits, dim=1)[0][0].item()
+    #
+    # # 打印文本与关键词的相似度分数
+    # print(f"与关键词 '{keyword}' 相似度分数：{score}")
+
+
+
+
+import re
+
+# ----   ——   ,  ，  !" ?  去掉
 if __name__ == '__main__':
+    # 读取原始文件
+    with open('../txts/save_word_txt/ce.txt', 'r', encoding='utf-8') as file:
+        original_text = file.readlines()
+        print(">>>>>",original_text)
 
+    # 定义正则表达式模式，用于匹配需要去除的字符\----\——\,\，\!"\?
+    pattern = r'[\“\”\.）’\.)\s<>(\-\—\,\，\!\?\"\'\、\=]+'
 
-    # 加载BERT模型和分词器
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model = BertForNextSentencePrediction.from_pretrained('bert-base-uncased')
+    # 定义正则表达式模式，用于匹配每行开头的大小写数字
+    digit_pattern = r'^[A-Za-z0-9]+\s*'
 
-    # 定义关键词
-    keyword = "apple"
-
-    # 示例文本
-    text = "I like bananas. Apples are delicious fruits. Orange is another fruit."
-
-    # 分词并编码
-    inputs = tokenizer(keyword, text, return_tensors='pt', padding=True, truncation=True)
-
-    # 进行文本相似度预测
-    outputs = model(**inputs)
-
-    # 获取相似度分数
-    score = torch.softmax(outputs.logits, dim=1)[0][0].item()
-
-    # 打印文本与关键词的相似度分数
-    print(f"与关键词 '{keyword}' 相似度分数：{score}")
-
-
-
-
-
-
-
-
-
+    # 打开新文件以写入处理后的内容
+    with open('../txts/save_word_txt/processed_text.txt', 'w', encoding='utf-8') as file:
+        # 遍历原始文件的每一行
+        for line in original_text:
+            # 使用正则表达式去除特定字符
+            clean_line = re.sub(pattern, '', line)
+            # 使用正则表达式去除每行开头的大小写数字
+            clean_line = re.sub(digit_pattern, '', clean_line)
+            # 去除句子两端的空白字符
+            clean_line = clean_line.strip()
+            # 如果处理后的行非空，则写入文件
+            if clean_line:
+                # 在句子末尾添加句号和换行符
+                for i in clean_line.split("。"):
+                    if i != "":
+                        # 使用正则表达式过滤掉 `·166·`
+                        clean_string = re.sub(r'·\d+·', '', i)
+                        file.write(clean_string + '。 \n')
 
 
 
--- utils.py
+++ utils.py
@@ -34,18 +34,15 @@ class DataTiramisu():
             save_file_word = dat_dict['save_file_word']
             if_all = dat_dict['if_all']
 
-
             # 等于True的情况下获取文件夹中的所有word文档，进行提取数据：
             if if_all == "True":
                 # 定义正则表达式模式，用于匹配需要去除的字符
                 pattern = r'[\“\”\.）’\.)\s<>(\-\—\,\，\!\?\"\'\、\=]+'
                 # 定义正则表达式模式，用于匹配每行开头的大小写数字
                 digit_pattern = r'^[A-Za-z0-9]+\s*'
-
                 dat_list = os.listdir(read_file_word)
                 for i in dat_list:
                     doc = docx.Document(f"{read_file_word}{i}")
-
                     for j in doc.paragraphs:
                         with open(save_file_word, 'a+', encoding='utf-8') as file:
                             # 使用正则表达式去除特定字符
@@ -62,7 +59,6 @@ class DataTiramisu():
                                     clean_string = re.sub(r'·\d+·|\d+\)', '', i)
                                     if i != "":
                                         file.write(clean_string + '。 \n')
-
             # 记录程序结束时间
             end_time = time.time()
             # 计算程序运行时间
@@ -73,116 +69,116 @@ class DataTiramisu():
             print(">>>>>>读取文件时出现异常！",e)
 
 
-    def post_pdf_txt(self,data_dict):
-
-        dat = data_dict
-        read_file_pdf = dat['read_file_pdf']
-        new_pdf_file_path =dat['new_pdf_file_path']
-        save_img_path =dat['save_img_path']
-        save_txt_path =dat['save_txt_path']
-        start_num =dat['start_num']
-        end_num =dat['end_num']
-        if_all = dat['if_all']
-
-        print(">>>>>>开始提取pdf中的数据")
-        # 记录程序开始时间
-        start_time = time.time()
-
-        # 读取文件夹下所有的pdf文件进行提取数据：
-
-        # 使用Tesseract OCR提取图片中的文字
-        pytesseract.pytesseract.tesseract_cmd = self.Tesseract
-
-        # 获取支持的语言列表
-        supported_languages = pytesseract.get_languages()
-        # print("Supported languages:", supported_languages)
-
-        try:
-            if if_all == "True":
-                #获取文件夹中所有数据
-            #     读取文件夹中所有pdf文件
-                pdf_list = os.listdir(read_file_pdf)
-                for i in pdf_list:
-                    # 将pdf文件中的数据转换为图片
-                    pages = convert_from_path(f"{read_file_pdf}{i}", 500)
-                    for page in pages:
-                        # print(">>>>>>",page)
-                        filename = f"{save_img_path}page_" + str(random.randint(100000,999999)) + ".jpg"
-                        # print(filename)
-                        page.save(filename, 'JPEG')
-
-            else:
-                # print(">>>>>6666我是false选项")
-            #     进行获取单个数据：
-            # 读取pdf文件：
-                pdfFile = open(read_file_pdf, 'rb')
-                pdf_input = PyPDF2.PdfReader(pdfFile)
-                # 创建一个新的pdf文件
-                pdf_output = PyPDF2.PdfWriter()
-                for i in range(int(start_num), int(end_num)):
-                    pdf_output.add_page(pdf_input.pages[i])
-                pdf_output.write(open(new_pdf_file_path, 'wb'))
-
-                # 将pdf转换为图片的形式进行操作：
-                pages = convert_from_path(new_pdf_file_path, 500)
-
-                image_counter = 1
-                for page in pages:
-                    filename = f"{save_img_path}page_" + str(image_counter) + ".jpg"
-                    page.save(filename, 'JPEG')
-                    image_counter += 1
-
-        except Exception as e:
-            print(">>>>>>操作单个或多个pdf文件异常",e)
-
-        try:
-            # 打开图片
-            file_list = os.listdir(save_img_path)
-            for filename in file_list:
-                # 读取图片数据:
-                image = Image.open(f"{save_img_path}{filename}")
-                # # 使用Tesseract OCR识别图片中的文字
-                text = pytesseract.image_to_string(image, lang=supported_languages[0])
-
-                # 定义正则表达式模式，用于匹配需要去除的字符
-                pattern = r'[\“\”\.）’\.)\s<>(\-\—\,\，\!\?\"\'\、\=]+'
-                # 定义正则表达式模式，用于匹配每行开头的大小写数字
-                digit_pattern = r'^[A-Za-z0-9]+\s*'
-
-                with open(f"{save_txt_path}", 'a+', encoding='utf-8') as file:
-                    # 使用正则表达式去除特定字符
-                    clean_line = re.sub(pattern, '', text.strip())
-                    # 使用正则表达式去除每行开头的大小写数字
-                    clean_line = re.sub(digit_pattern, '', clean_line)
-                    # 去除句子两端的空白字符
-                    clean_line = clean_line.strip()
-                    # 如果处理后的行非空，则写入文件
-                    if clean_line:
-                        # 在句子末尾添加句号和换行符
-                        for i in clean_line.split("。"):
-                            clean_string = re.sub(r'·\d+·|\d+\)', '', i)
-                            if i != "":
-                                file.write(clean_string + '。 \n')
-
-            print(">>>>>>提取pdf数据完成\n\n")
-
-        #       读取完后将新创建的文件进行删除：
-        #    删除新创建pdf文件：
-            if if_all == "False":
-                os.remove(new_pdf_file_path)
-        #     删除静态图片数据：
-            for r_i in file_list:
-                os.remove(f"{save_img_path}{r_i}")
-
-            # 记录程序结束时间
-            end_time = time.time()
-            # 计算程序运行时间
-            running_time = end_time - start_time
-            print(">>>>>>提取数据操作结束,程序运行时间：", running_time, "秒")
-            print("\n\n\n")
-
-        except Exception as e:
-            print(">>>>>>读取图片文件失败！",e)
+    # def post_pdf_txt(self,data_dict):
+    #
+    #     dat = data_dict
+    #     read_file_pdf = dat['read_file_pdf']
+    #     new_pdf_file_path =dat['new_pdf_file_path']
+    #     save_img_path =dat['save_img_path']
+    #     save_txt_path =dat['save_txt_path']
+    #     start_num =dat['start_num']
+    #     end_num =dat['end_num']
+    #     if_all = dat['if_all']
+    #
+    #     print(">>>>>>开始提取pdf中的数据")
+    #     # 记录程序开始时间
+    #     start_time = time.time()
+    #
+    #     # 读取文件夹下所有的pdf文件进行提取数据：
+    #
+    #     # 使用Tesseract OCR提取图片中的文字
+    #     pytesseract.pytesseract.tesseract_cmd = self.Tesseract
+    #
+    #     # 获取支持的语言列表
+    #     supported_languages = pytesseract.get_languages()
+    #     # print("Supported languages:", supported_languages)
+    #
+    #     try:
+    #         if if_all == "True":
+    #             #获取文件夹中所有数据
+    #         #     读取文件夹中所有pdf文件
+    #             pdf_list = os.listdir(read_file_pdf)
+    #             for i in pdf_list:
+    #                 # 将pdf文件中的数据转换为图片
+    #                 pages = convert_from_path(f"{read_file_pdf}{i}", 500)
+    #                 for page in pages:
+    #                     # print(">>>>>>",page)
+    #                     filename = f"{save_img_path}page_" + str(random.randint(100000,999999)) + ".jpg"
+    #                     # print(filename)
+    #                     page.save(filename, 'JPEG')
+    #
+    #         else:
+    #             # print(">>>>>6666我是false选项")
+    #         #     进行获取单个数据：
+    #         # 读取pdf文件：
+    #             pdfFile = open(read_file_pdf, 'rb')
+    #             pdf_input = PyPDF2.PdfReader(pdfFile)
+    #             # 创建一个新的pdf文件
+    #             pdf_output = PyPDF2.PdfWriter()
+    #             for i in range(int(start_num), int(end_num)):
+    #                 pdf_output.add_page(pdf_input.pages[i])
+    #             pdf_output.write(open(new_pdf_file_path, 'wb'))
+    #
+    #             # 将pdf转换为图片的形式进行操作：
+    #             pages = convert_from_path(new_pdf_file_path, 500)
+    #
+    #             image_counter = 1
+    #             for page in pages:
+    #                 filename = f"{save_img_path}page_" + str(image_counter) + ".jpg"
+    #                 page.save(filename, 'JPEG')
+    #                 image_counter += 1
+    #
+    #     except Exception as e:
+    #         print(">>>>>>操作单个或多个pdf文件异常",e)
+    #
+    #     try:
+    #         # 打开图片
+    #         file_list = os.listdir(save_img_path)
+    #         for filename in file_list:
+    #             # 读取图片数据:
+    #             image = Image.open(f"{save_img_path}{filename}")
+    #             # # 使用Tesseract OCR识别图片中的文字
+    #             text = pytesseract.image_to_string(image, lang=supported_languages[0])
+    #
+    #             # 定义正则表达式模式，用于匹配需要去除的字符
+    #             pattern = r'[\“\”\.）’\.)\s<>(\-\—\,\，\!\?\"\'\、\=]+'
+    #             # 定义正则表达式模式，用于匹配每行开头的大小写数字
+    #             digit_pattern = r'^[A-Za-z0-9]+\s*'
+    #
+    #             with open(f"{save_txt_path}", 'a+', encoding='utf-8') as file:
+    #                 # 使用正则表达式去除特定字符
+    #                 clean_line = re.sub(pattern, '', text.strip())
+    #                 # 使用正则表达式去除每行开头的大小写数字
+    #                 clean_line = re.sub(digit_pattern, '', clean_line)
+    #                 # 去除句子两端的空白字符
+    #                 clean_line = clean_line.strip()
+    #                 # 如果处理后的行非空，则写入文件
+    #                 if clean_line:
+    #                     # 在句子末尾添加句号和换行符
+    #                     for i in clean_line.split("。"):
+    #                         clean_string = re.sub(r'·\d+·|\d+\)', '', i)
+    #                         if i != "":
+    #                             file.write(clean_string + '。 \n')
+    #
+    #         print(">>>>>>提取pdf数据完成\n\n")
+    #
+    #     #       读取完后将新创建的文件进行删除：
+    #     #    删除新创建pdf文件：
+    #         if if_all == "False":
+    #             os.remove(new_pdf_file_path)
+    #     #     删除静态图片数据：
+    #         for r_i in file_list:
+    #             os.remove(f"{save_img_path}{r_i}")
+    #
+    #         # 记录程序结束时间
+    #         end_time = time.time()
+    #         # 计算程序运行时间
+    #         running_time = end_time - start_time
+    #         print(">>>>>>提取数据操作结束,程序运行时间：", running_time, "秒")
+    #         print("\n\n\n")
+    #
+    #     except Exception as e:
+    #         print(">>>>>>读取图片文件失败！",e)
 
 
 
