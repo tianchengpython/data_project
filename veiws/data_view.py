49bd85178d9aeb954ad2a70b1767af6d45242a9b
tiancheng <tian41848@gmail.com>
2024-03-27 13:51:45 +0800

    1.8v

--- veiws/data_view.py
+++ veiws/data_view.py
@@ -5,29 +5,28 @@ import json
 from utils import DataTiramisu
 from models.db_model import insert_data,select_data
 
-
 class DataView(DataTiramisu):
     # 提取pdf中的数据：
-    # def get_pdf_data(self):
-    #     read_file_pdf = input("》》》》》》请输入读取文件路径：").strip()
-    #     new_pdf_file_path = input("》》》》》》请输入读新创建pdf路径，单个pdf才使用到：")
-    #     save_img_path = input("》》》》》》请输入存放要提取数据图片：")
-    #     save_txt_path = input("》》》》》》请输入存放提取后的文本存放地址：")
-    #     start_num = input("》》》》》》请输入单个pdf使用，开始索引：")
-    #     end_num = input("》》》》》》请输入单个pdf使用，结束索引：")
-    #     if_all = input("》》》》》》请输入True读取文件夹中所有数据")
-    #
-    #     data_dict = {
-    #         "read_file_pdf": read_file_pdf,          # 读取文件路径
-    #         "new_pdf_file_path": new_pdf_file_path,  # 读取单个pdf才使用到
-    #         "save_img_path": save_img_path,          # 存放要提取数据图片
-    #         "save_txt_path": save_txt_path,          # 存放提取后的文本存放地址
-    #         "start_num": start_num,                  # 单个pdf使用：开始
-    #         "end_num": end_num,                      # 单个pdf使用：结束
-    #         "if_all": if_all                         # True读取文件夹中所有数据，False读取单个
-    #     }
-    #
-    #     self.post_pdf_txt(data_dict)
+    def get_pdf_data(self):
+        read_file_pdf = input("》》》》》》请输入读取文件路径：").strip()
+        new_pdf_file_path = input("》》》》》》请输入读新创建pdf路径，单个pdf才使用到：")
+        save_img_path = input("》》》》》》请输入存放要提取数据图片：")
+        save_txt_path = input("》》》》》》请输入存放提取后的文本存放地址：")
+        start_num = input("》》》》》》请输入单个pdf使用，开始索引：")
+        end_num = input("》》》》》》请输入单个pdf使用，结束索引：")
+        if_all = input("》》》》》》请输入True读取文件夹中所有数据,False读取单个：")
+
+        data_dict = {
+            "read_file_pdf": read_file_pdf,          # 读取文件路径
+            "new_pdf_file_path": new_pdf_file_path,  # 读取单个pdf才使用到
+            "save_img_path": save_img_path,          # 存放要提取数据图片
+            "save_txt_path": save_txt_path,          # 存放提取后的文本存放地址
+            "start_num": start_num,                  # 单个pdf使用：开始
+            "end_num": end_num,                      # 单个pdf使用：结束
+            "if_all": if_all                         # True读取文件夹中所有数据，False读取单个
+        }
+
+        self.post_pdf_txt(data_dict)
 
 
     # 提取world中的数据：
@@ -72,11 +71,11 @@ if __name__ == '__main__':
     dat = DataView()
 
     # 提取pdf中的数据：
-    procedure = input(">>>>>>请输入程序选项：\n1提取word中书本数据：\n2读取要提问的问题：")
+    procedure = input(">>>>>>请输入程序选项：\n1提取pdf中书本数据:\n2提取word中书本数据：\n3读取要提问的问题：")
 
-    # if procedure == "1":
-    #     dat.get_pdf_data()
     if procedure == "1":
+        dat.get_pdf_data()
+    if procedure == "2":
         dat.get_world_data()
     elif procedure == "3":
         dat.transition()
